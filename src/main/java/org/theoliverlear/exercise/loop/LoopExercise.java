f40dba8005e231778a25750fd9ae136ba7f9a7f5
theoliverlear <sigwartholiver2@gmail.com>
2025-04-08 14:37:36 -0500

    Update LoopExercise.java to have matching winning numbers for a more effective exercise

--- src/main/java/org/theoliverlear/exercise/loop/LoopExercise.java
+++ src/main/java/org/theoliverlear/exercise/loop/LoopExercise.java
@@ -3,7 +3,7 @@ package org.theoliverlear.exercise.loop;
 public class LoopExercise {
     //============================-Variables-=================================
     static int[] winningNumbers = {55, 23, 12, 8, 44, 19};
-    static int[] lotteryNumbers = {29, 14, 97, 5, 85, 67};
+    static int[] lotteryNumbers = {29, 14, 12, 5, 85, 19};
     static int dollarsPerNumber = 2000;
     //--------------------------Solve-With-Loop-------------------------------
     public static void printWinnings() {
